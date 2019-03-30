bc3b1095ed425dcc6c66f7a8a7a3ad144039582f
neo4dev <neo4dev@qq.com>
2019-03-30 14:38:31 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from fastai.vision import *
 export_file_url = 'https://drive.google.com/uc?export=download&id=1R-cYAzuyQi3MpFpTN61cZxyUy1mPM6gG'
 export_file_name = 'export.pkl'
 
-classes = ['bosi', 'eluosi', 'xianluo']
+classes = ['bosi', 'eluosi', '暹罗猫']
 path = Path(__file__).parent
 
 app = Starlette()
